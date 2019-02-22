11b83bc863a294a3f71a780b30accd48120ac92c
Matt Ray <matthewhray@gmail.com>
2019-02-22 14:56:13 +1100

    Add nicer output for the controls.
    Signed-off-by: Matt Ray <matthewhray@gmail.com>

--- controls/default.rb
+++ controls/default.rb
@@ -1,9 +1,23 @@
-title 'Uptime'
-
-describe file('/proc/uptime') do
-  it { should exist }
+control 'Uptime exists' do
+  title '/proc/uptime exists'
+  desc "
+Can't check uptime without the uptime file
+  "
+  impact 1.0
+  tag 'uptime', 'proc'
+  describe file('/proc/uptime') do
+    it { should exist }
+  end
 end
 
-describe command("awk '{print $1}' /proc/uptime") do
-  its('stdout') { should cmp > '3600' }
+control 'Up for an hour' do
+  title '> 3600 seconds '
+  desc "
+Check that the machine has been up for an hour.
+  "
+  impact 1.0
+  tag 'uptime'
+  describe command("awk '{print $1}' /proc/uptime") do
+    its('stdout') { should cmp > '3600' }
+  end
 end
